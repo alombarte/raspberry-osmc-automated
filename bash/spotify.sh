fac582acb858c091b73061a4e3e21c768ec7266d
Albert Lombarte <alombarte@zinio.com>
2015-11-04 20:46:58 +0100

    Additional information

--- bash/spotify.sh
+++ bash/spotify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 # Spotify plugin
+# The Spotify plugin does not work with Spotify Unlimited plan (old), only Premium accounts.
 curl -O http://azkotoki.org/download-file/36/script-audio-spotimc-1-0-beta5 ~/script-audio-spotimc-1-0-beta5.zip
 
 echo "Go to Settings > Addons > Install from ZIP"
